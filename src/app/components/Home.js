7bbed79a62dc66f4f8e672cf91506ce47a2c94cb
Vadim Developer <netuns@gmail.com>
2017-08-19 17:30:59 -0400

    Up to React props

---
+++ src/app/components/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+//js exprssion: {}
+//this.props is from React.Component
+//<li key={i}>{hobby}</li> , key is to give unique id. i is iteration numnber form 0
+export class Home extends React.Component {
+    render() {
+        var text = "Hey";
+        return (
+            <div>
+                <p>In a new Component!</p>
+                <p>{text}</p>
+                <p>You name is: {this.props.name}, you age is {this.props.age}</p>
+                <p>Usr Object => name {this.props.user.name} </p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul> 
+                        {this.props.user.hobbies.map((hobby, i ) => <li key={i}>{hobby}</li>)}
+                    </ul>  
+                </div>
+                {this.props.children}
+
+            </div>
+
+        );
+    }
+}
+
+Home.propTypes = {
+    name: React.PropTypes.string,
+    age: React.PropTypes.number,
+    use: React.PropTypes.object,
+    children: React.PropTypes.isRequired
+}
