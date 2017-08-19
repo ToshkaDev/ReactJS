f01a7d06e2c74f676a4c63a280b6792298372ab7
Vadim Developer <netuns@gmail.com>
2017-08-19 17:30:59 -0400

    Up to React props

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,36 @@
-console.log("It works!");
+import React from "react";
+import { render } from "react-dom";
+
+//We are in /app folder
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+
+class App extends React.Component {
+    render() {
+        var user  = {
+            name: "Anna",
+            hobbies: ["Sport", "Reading"]
+        }
+        return (
+            //workgin here with bootstrap
+            <div className="containr">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header/>
+                    </div>
+                </div>
+            <div className="row">
+                <div className="col-xs-10 col-xs-offset-1">
+                    <Home name={"Vadim"} age={32} user={user}>
+                        <p>This is a paragraph</p>    
+                    </Home>
+                </div>
+            </div>
+
+            </div>
+        )
+    }
+} 
+//We are rendering this App class in div element with id = "app" in index.html
+render(<App/>, window.document.getElementById("app"));
