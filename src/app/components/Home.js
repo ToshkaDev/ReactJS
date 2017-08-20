5a29c67f85f101fdc228d808b2ce811140536c72
Vadim Developer <netuns@gmail.com>
2017-08-20 15:03:53 -0400

    Some little corrections

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,8 +3,9 @@ import React from "react";
 //js exprssion: {}
 //this.props is from React.Component
 //<li key={i}>{hobby}</li> , key is to give unique id. i is iteration numnber form 0
-//onClick={this.makeOlder.bind(this)} or onClick={{() => this.makeOlder()}. I need this, just this.makeOlder will not work, beacuse this will refer
+//onClick={this.makeOlder.bind(this)} or onClick={() => this.makeOlder()}. I need this, just this.makeOlder will not work, beacuse this will refer
 //not to class object Home buut to button.
+//But this still will not be rendered. It will be rendered when th 'state' will change
 export class Home extends React.Component {
     constructor(props) {
         super();
@@ -20,7 +21,7 @@ export class Home extends React.Component {
         return (
             <div>
                 <p>In a new Component!</p>
-                <p>You name is: {this.props.name}, you age is {this.props.age}</p>
+                <p>You name is: {this.props.name}, you age is {this.age}</p>
                 <hr/>
                 <button onClick={() => this.makeOlder()} className="btn btn-primary"> Make me older </button>
             </div>
